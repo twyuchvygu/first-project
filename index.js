7d57f6bcea29fd61647d9cc46eda0c4b042740ff
twyuchvygu <gs1iwantaol@hotmail.com>
2024-05-10 21:23:33 +0700

    aptos

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-const factorial = (num) => {
-  if (num === 0) return 1;
-  return num * factorial(num - 1);
-};
+function countAndSay(n) {
+  let result = "1";
+  for (let i = 1; i < n; i++) {
+    let temp = "";
+    let count = 1;
+    for (let j = 0; j < result.length; j++) {
+      if (result[j] === result[j + 1]) {
+        count++;
+      } else {
+        temp += count + result[j];
+        count = 1;
+      }
+    }
+    result = temp;
+  }
+  return result;
+}
