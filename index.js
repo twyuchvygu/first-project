99160dd61b6183c4be8feeda7be973d265f5a53e
twyuchvygu <gs1iwantaol@hotmail.com>
2024-05-11 18:33:28 +0700

    update aptos task

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-function countAndSay(n) {
-  let result = "1";
-  for (let i = 1; i < n; i++) {
-    let temp = "";
-    let count = 1;
-    for (let j = 0; j < result.length; j++) {
-      if (result[j] === result[j + 1]) {
-        count++;
-      } else {
-        temp += count + result[j];
-        count = 1;
-      }
-    }
-    result = temp;
-  }
-  return result;
-}
+const chunk = (arr, size) =>
+  Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
+    arr.slice(i * size, i * size + size),
+  );
