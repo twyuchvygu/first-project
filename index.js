fc25ffc84c2ca24621301949c08416ed2187f907
twyuchvygu <gs1iwantaol@hotmail.com>
2024-05-14 08:34:26 +0700

    hello world

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const zip = (arr1, arr2) => arr1.map((el, i) => [el, arr2[i]]);
+function maxProfit(prices) {
+  let minPrice = Infinity;
+  let maxProfit = 0;
+  for (const price of prices) {
+    minPrice = Math.min(minPrice, price);
+    maxProfit = Math.max(maxProfit, price - minPrice);
+  }
+  return maxProfit;
+}
