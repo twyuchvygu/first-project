b40e005761c3bc9a07127bd05b1484a49b7cc66b
twyuchvygu <gs1iwantaol@hotmail.com>
2024-05-16 02:04:11 +0700

    update sdk task commit

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-function maxProfit(prices) {
-  let minPrice = Infinity;
-  let maxProfit = 0;
-  for (const price of prices) {
-    minPrice = Math.min(minPrice, price);
-    maxProfit = Math.max(maxProfit, price - minPrice);
+function wiggleSort(nums) {
+  nums.sort((a, b) => a - b);
+  const median = Math.floor((nums.length + 1) / 2);
+  const left = nums.slice(0, median);
+  const right = nums.slice(median);
+  for (let i = 0; i < nums.length; i++) {
+    if (i % 2 === 0) nums[i] = left.pop();
+    else nums[i] = right.pop();
   }
-  return maxProfit;
 }
